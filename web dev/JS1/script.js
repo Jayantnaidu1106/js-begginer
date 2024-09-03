58d5111fff4594527cb8d4f1740c1453c3ec0f19
Jayantnaidu1106 <155427377+Jayantnaidu1106@users.noreply.github.com>
2024-09-03 20:54:41 +0530

    Add files via upload

---
+++ web dev/JS1/script.js
@@ -0,0 +1,31 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+buttons.forEach(function (button) {
+    button.addEventListener('click', function(e)
+{
+    if(e.target.id === 'yellow'){
+        body.style.backgroundColor = e.target.id;
+    }
+    
+
+    if(e.target.id === 'grey'){
+        body.style.backgroundColor = e.target.id;
+    }
+
+    if(e.target.id === 'green'){
+        body.style.backgroundColor = e.target.id;
+    }
+    
+
+    if(e.target.id === 'red'){
+        body.style.backgroundColor = e.target.id;
+    }
+    
+
+    if(e.target.id === 'orange'){
+        body.style.backgroundColor = e.target.id;
+    }
+    
+});
+
+});
